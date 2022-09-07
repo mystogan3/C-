30392b07873be2e64f17f7593de0496cff120e90
PoliakovAY <myatogan3@yandex.ru>
2022-09-08 00:43:09 +0300

    Add Homework for seminar6 C#

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел происходит через Enter, останавливается при введении слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+using System;
+
+void GetPositiveCount()
+{
+    int count = 0;
+    while (true)
+    {
+        Console.Write("Enter a number: ");
+        string value = Console.ReadLine();
+        if (value.ToLower() == "stop") break;
+        int TempValue=0;
+        if (int.TryParse(value, out TempValue))
+        {
+            if (TempValue > 0) count++;
+        }
+        else continue;
+    }
+    Console.WriteLine($"You entered {count} positives numbers");
+}
+GetPositiveCount();
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+void GetPoint (double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 ) 
+    {
+        Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} --> нет точек пересечения");
+        return;
+    }
+    double x = (b2-b1)/(k1-k2);
+    double y = (k2*(b2-b1))/(k1-k2)+b2;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} --> ({x}; {y})");
+}
+
+Console.Write("Enter b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+GetPoint(b1,k1,b2,k2);
